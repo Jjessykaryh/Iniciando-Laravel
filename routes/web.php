5cce3c7d07c8f32b69268976a9bd3cd8b14d559e
Jjessykaryh <mariana.pachay1988@gmail.com>
2022-03-12 22:55:16 -0500

    implementando roles y permisos

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/inicio', function () {
+    return view('principal.inicio');
+})->name('principal');
